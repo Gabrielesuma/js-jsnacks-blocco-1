23d452f46ab163c0d3d156060dcc22264231b3fd
Gabrielesuma <gabrielesuma139@gmail.com>
2024-03-12 16:33:05 +0100

    array bonus

---
+++ array-bonus/js/script.js
@@ -0,0 +1,8 @@
+let arr1 = ['tavolo', 'sedia', 'televisione', 'divano', 'poltrona'];
+let arr2 = ['arancia', 'mela', 'pera'];
+
+for(i = 0; i < 2; i++){
+    arr2.push(prompt('inserisci un nuovo elemento'));
+}
+
+console.log(arr1, arr2);
