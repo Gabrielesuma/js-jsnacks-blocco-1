f87c29e5f3de330a6b1eecb8d06716336cad2052
Gabrielesuma <gabrielesuma139@gmail.com>
2024-03-12 16:11:24 +0100

    array tail complete

--- array-tail/js/script.js
+++ array-tail/js/script.js
@@ -8,7 +8,9 @@ let num = parseInt(prompt('quanti elementi vuoi inserire?'));
 for(i = 0; i < num; i++){
     arr.push(random(1,100));
 }
-//console.log(arr);
-if(num >= 5){
-    console.log(arr[i]);
+console.log(arr);
+if(num > 5){
+    for(i = num; i >= num - 5; i--){
+        console.log(arr[i]);
+    }
 }
